221fa2560508017d98d562ded7cab97183d4253d
Dennis <dequaderer@gmail.com>
2021-03-16 13:31:33 +0100

    Resolve exercise 2

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ example_urls = ['https://volgograd.masterdel.ru/master/byttehnika/', 'https://ma
 
 print('**********Exercise 1**********')
 for example_url in example_urls:
-    print(analyzer.get_phones_from_url(example_url))
+    print(example_url + ' : ' + str(analyzer.get_phones_from_url(example_url)))
 
+
+print('**********Exercise 2**********')
+input_array = [1, 4, 7, 6, 6, 2, 8, 12, 4]
+output_array = list(sorted(set(input_array), key=input_array.index))
+print('input array:', str(input_array))
+print('output array:', str(output_array))
