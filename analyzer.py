d0ac3cc7704c7f9ee18246a3880d3055bdf4bec7
Dennis <dequaderer@gmail.com>
2021-03-16 13:20:20 +0100

    Fix regex

--- analyzer.py
+++ analyzer.py
@@ -12,6 +12,6 @@ def get_phones_from_url(url):
 
 def get_phones_from_text(text):
     """Return a list of numbers that follow format 8KKKNNNNNNN, extracted from given text"""
-    pattern = r'[8]{1}[\s]?[(]?[\d+]{3}[)]? [\d+]{3}[-][\d+]{2}[\d+]?'
+    pattern = r'[8]{1}[\s][(]?[\d+]{3}[)]?[\s][\d+]{3}[-]?[\d+]{2}[-]?[\d+]{2}'
     numbers = re.findall(pattern, text)
     return numbers
