8c0c3b208aae283737cb3e0e8cee72492fa1e570
Dennis <dequaderer@gmail.com>
2021-03-16 12:58:23 +0100

    Add method to get phones from given url

--- crawl.py
+++ crawl.py
@@ -2,6 +2,14 @@ import requests
 import re
 
 
+def get_phones_from_url(url):
+    """Return the list of phone numbers with format 8KKKNNNNNNN, in a given url"""
+    r = requests.get(url)
+    numbers = get_phones_from_text(r.text)
+    numbers_set = set(numbers)  # remove duplicates
+    return numbers_set
+
+
 def get_phones_from_text(text):
     """Return a list of numbers that follow format 8KKKNNNNNNN, extracted from given text"""
     pattern = r'[8]{1}[\s]?[(]?[\d+]{3}[)]? [\d+]{3}[-][\d+]{2}[\d+]?'
